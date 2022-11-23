b66451f330ed4ba6e5d05efd6963fbf2765363e6
ddatdt12 <ddatdt12@gmail.com>
2022-11-23 23:35:10 +0700

    add authentication, send message

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,69 @@
-import React, { useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
 import "./App.css";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
   const [connection, setConnection] = useState();
   const [messages, setMessages] = useState([]);
-  const userNameInputRef = useRef(null);
-  const roomInputRef = useRef(null);
+  const [auth, setAuth] = useState({ user: null, token: null });
+  const receiverInputRef = useRef(null);
   const messageInputRef = useRef(null);
 
-  const joinRoom = async () => {
-    const userName = userNameInputRef.current.value;
+  useEffect(() => {
+    try {
+      const savedAuth = localStorage.getItem("auth");
+      if (savedAuth) {
+        setAuth(JSON.parse(savedAuth));
+      }
+    } catch (error) {
+      localStorage.removeItem("auth");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (auth.user && auth.token) {
+      joinRealTime();
+    }
+  }, [auth?.token]);
+  const joinRealTime = useCallback(async () => {
     // const room = roomInputRef.current.value;
+    if (!auth.token) {
+      return;
+    }
     try {
       const connection = new HubConnectionBuilder()
-        .withUrl("https://localhost:7113/chat")
+        .withUrl("https://localhost:7113/hubs/chat", {
+          accessTokenFactory: () => auth.token,
+        })
         .configureLogging(LogLevel.Information)
         .build();
-
       connection.on("ReceiveMessage", (message) => {
         console.log("Comming message: ", message);
-        // setMessages((messages) => [...messages, { ...message }]);
+        setMessages((messages) => [...messages, message]);
       });
 
       connection.onclose((e) => {
+        console.log("e", e);
         setConnection();
         setMessages([]);
       });
 
       await connection.start();
-      await connection.invoke("JoinRealtime", userName);
+      await connection.invoke("JoinRealtime");
 
       setConnection(connection);
     } catch (e) {
-      console.log(e);
+      console.log("check error: ", e);
     }
-  };
+  }, [auth.token]);
 
   const sendMessage = async (message) => {
     try {
-      await connection.invoke("SendMessage", { content: message });
+      await connection.invoke("SendMessage", {
+        content: message,
+        receiverId: receiverInputRef.current.value,
+      });
     } catch (e) {
       console.log(e);
     }
@@ -57,19 +81,23 @@ const App = () => {
     <div className="app">
       <h2>MyChat</h2>
       <hr className="line" />
-      {!connection ? (
+      {!auth?.user && (
+        <LoginForm
+          onSuccess={(auth) => {
+            setAuth(auth);
+            localStorage.setItem("auth", JSON.stringify(auth));
+          }}
+        />
+      )}
+      {auth?.user && connection && (
         <div>
-          <input type="text" ref={userNameInputRef} placeholder="User" />
-          <input type="text" ref={roomInputRef} placeholder="Room" />
-          <button onClick={joinRoom}>Join</button>
-        </div>
-      ) : (
-        <div>
-          {userNameInputRef.current.value} Joined
+          <input type="text" ref={receiverInputRef} placeholder="ReceiverId" />
+          {/* <button onClick={joinRoom}>Join</button> */}
+          <div>{JSON.stringify(auth.user)} Joined</div>
           <div>
             {messages.map((message, index) => (
-              <p key={index}>
-                {message.userId} : {message.message}
+              <p key={message.id}>
+                {message.senderId} : {message.content}
               </p>
             ))}
           </div>
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,35 @@
+import React, { useRef } from "react";
+
+const LoginForm = ({ onSuccess }) => {
+  const emailInputRef = useRef(null);
+  const passwordInputRef = useRef(null);
+  const handleLogin = async () => {
+    //post login with fetch
+    const email = emailInputRef.current.value;
+    const password = passwordInputRef.current.value;
+    try {
+      const res = await fetch("https://localhost:7113/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      }).then((res) => res.json());
+
+      console.log(res);
+      onSuccess({ user: res.data, token: res.token });
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  };
+
+  return (
+    <div>
+      <input type="text" ref={emailInputRef} placeholder="Email" />
+      <input type="text" ref={passwordInputRef} placeholder="Password" />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default LoginForm;
